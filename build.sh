3d9abe9e0527c7fab9ce2d55486316da41170d7d
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2024-02-28 16:21:49 +0100

    Add build script and update main.py to handle frozen state

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Make sure to have pyinstaller installed
+# This will generate a binary file named "main" inside a folder named "dist"
+pyinstaller --onefile main.py
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from controllers.TextSummaryController import TextSummaryController
 import sys
+from pathlib import Path
 
 if __name__ == "__main__":
 	app = QGuiApplication(sys.argv)
@@ -12,7 +13,14 @@ if __name__ == "__main__":
 	context = engine.rootContext()
 	context.setContextProperty("textSummaryController", textSummaryController)
 
-	engine.load("views/main.qml")
+	# Define the base path depending on whether we're frozen (via PyInstaller)
+	if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+		base_path = Path(sys._MEIPASS) # type: ignore
+	else:
+		base_path = Path(__file__).resolve().parent
+
+	qml_file_path = base_path / 'views' / 'main.qml'
+	engine.load(qml_file_path)
 
 	if not engine.rootObjects():
 		sys.exit(-1)
